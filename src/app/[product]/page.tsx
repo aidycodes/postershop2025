f1967a4dc905bc0e3e25634bbea4b77d69f4534e
aidy <aidycodes@gmail.com>
2025-02-08 17:18:40 +0000

    first commit

--- src/app/[product]/page.tsx
+++ src/app/[product]/page.tsx
@@ -1,6 +1,6 @@
 
-const ProductPage = async({params}: {params: {product: string}}) => {
-    const { product } = await params
+const ProductPage = async(props: {params: {product: string}}) => {
+    const { product } = await props.params
     console.log({product})
  const products = await fetch(`http://localhost:3000/api/products/productById/?id=${product}`)
   return <div>ProductPage</div>
