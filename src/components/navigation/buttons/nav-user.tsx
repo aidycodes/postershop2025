bbd1210b27a3583a173503209c1392bb6b4242b2
aidy <aidycodes@gmail.com>
2025-02-15 14:55:32 +0000

    minor fixes for SSR and unused values

--- src/components/navigation/buttons/nav-user.tsx
+++ src/components/navigation/buttons/nav-user.tsx
@@ -5,7 +5,7 @@ const NavUser = ({isSignedIn, name}: {isSignedIn: boolean, name?: string}) => {
     return (
         <div>            
                 {isSignedIn ? (       
-                    <Link href="/users/profile">
+                    <Link href="/dashboard/user">
                     <div className="flex items-center space-x-1 text-gray-600 hover:text-gray-900 cursor-pointer">
                         <User className="h-6 w-6" />
                         <span>{name}</span>
