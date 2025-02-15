0d2f2fdb334f9ab7c2cb27298559b78ac2fc3a76
aidy <aidycodes@gmail.com>
2025-02-15 14:19:58 +0000

    minor fixes for SSR and unused values

--- src/app/api/v1/events/stripe/route.ts
+++ src/app/api/v1/events/stripe/route.ts
@@ -1,28 +1,7 @@
-import { NextResponse, NextRequest } from 'next/server';
-import Stripe from 'stripe';
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+import { NextRequest } from 'next/server';
 
  const handler = async (req: NextRequest) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price_data: {
-              currency: 'gbp',
-              product_data: {
-                name: 'Sci-fi space station - XLarge',
-              },
-              unit_amount: 2000,
-            },
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: 'http://localhost:4242/success',
-        cancel_url: 'http://localhost:4242/cancel',
-      });
-    console.log(session)
-      return NextResponse.json(session.url)
+    console.log(req.body)
     }
 
 export { handler as GET }
